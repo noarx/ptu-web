f2f3dfbf0ed462e736a97088e44e6b635379c1df
noarx <noarxjp@gmail.com>
2025-06-20 15:49:31 -0600

    Add test Pokemon object

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,6 @@
 // Main Web-app Logic
+
+import { Pokemon } from "./classes/pokemon";
+
+// Test Pokemon Object Implementation
+let missingno = new Pokemon("missingno",1,2,3,4,5,6,"normal",null,"test ability 1","test ability 2","test ability adv1","test ability adv2","test ability high", null, null, 20, 25, null, null, null, null)
