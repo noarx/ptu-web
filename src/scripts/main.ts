46ecff6955950e9fff422d0af013b51cca129db9
noarx <noarxjp@gmail.com>
2025-06-22 11:36:58 -0600

    Create test Pokemon object

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,4 +3,28 @@
 import { Pokemon } from "./classes/pokemon";
 
 // Test Pokemon Object Implementation
-let missingno = new Pokemon("missingno",1,2,3,4,5,6,"normal",null,"test ability 1","test ability 2","test ability adv1","test ability adv2","test ability high", null, null, 20, 25, null, null, null, null)
+
+const missingno = new Pokemon({
+    pkmn_name: "missingno",
+    hp: 1,
+    atk: 2,
+    def: 3,
+    sp_atk: 4,
+    sp_def: 5,
+    speed: 6,
+    type1: "normal",
+    type2: null,
+    basic_ability1: "test ability 1",
+    basic_ability2: "test ability 2",
+    adv_ability1: "test ability adv1",
+    adv_ability2: "test ability adv2",
+    high_ability: "test ability high",
+    evo_name: null, 
+    evo_level: null, 
+    height: 20,
+    weight: 25, 
+    gender_ratio: null,
+    egg_group1: null,
+    egg_group2: null, 
+    hatch_rate: null
+});
